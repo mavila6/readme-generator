e859ddbe4eb1e5440ca29f4fc045563e6ecc860a
Marco Avila <mavila6@email.arizona.com>
2021-07-06 01:14:52 -0700

    finished the template for the md file to be generated

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,5 +16,40 @@ module.exports = response => {
     - [Contributing](#contributing)
     - [Tests](#tests)
     - [Questions](#questions)
+
+    
+    ### Installation:
+    
+    To install necessary dependencies, run the following code in the console:
+
+    \`\`\`${response.installation}\`\`\`
+
+    ### Usage:
+
+    ${response.usage}
+
+    ### License: 
+    
+    This project is licensed under:
+    
+    ${response.license}
+
+    ### Contributing: 
+
+    ${response.contribute}
+
+    ### Tests: 
+    
+    To test, run the following code in the console:
+
+    \`\`\`${response.test}\`\`\`
+
+    ### Questions:
+
+    If you have any questions feel free to take a look at my [GitHub](https://github.com/${response.username}) or contact me at ${response.email}
+
+    ![Picture](https://github.com/${response.username}.png?size=75)
+
+    &copy; ${new Date().getFullYear()} ${response.author}
     `
 }
