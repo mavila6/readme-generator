b3ca53f4c0b1dc8ea6963e07ecbac9634ca028fb
Marco Avila <mavila6@email.arizona.com>
2021-07-06 03:18:49 -0700

    added inquirer prompt syntax

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const generateMd = require('./utils/generateMarkdown');
 const promptUser = () => {
     return inquirer.prompt([
         {
-        
+        type: "input",
+        name: "author",
+        message: "Please enter the name of the author (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+            return true;
+            } else {
+            console.log('Must enter a name.');
+            return false;
+            }
         }
+        } 
     ]);
 };
