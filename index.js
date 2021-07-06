ea9ce17ce308d0b8f89edafe02bd098fa9083926
Marco Avila <mavila6@email.arizona.com>
2021-07-05 20:31:44 -0700

    added variables used to require node npm modules and additional js file

---
+++ index.js
@@ -0,0 +1,4 @@
+//variables created to require nppm node modules and additional js page//
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMd = require('./utils/generateMarkdown');
